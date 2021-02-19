2e6998442134ada94f7a36ddc5f61bc90837edf2
eyast <eyast@microsoft.com>
2021-02-19 16:14:51 +1100

    testing unit testing

--- hello_app/tests/test_root2.py
+++ hello_app/tests/test_root2.py
@@ -4,3 +4,9 @@ def test_landing(client):
     landing = client.get("/")
     html = landing.data.decode()
     assert landing.status_code == 200
+
+def test_temperature(client):
+    temperature = client.get("/temp")
+    html = temperature.data.decode()
+    print(html)
+    assert type(html) == int
